b125917886c4760c40cb63f3bab5e92e79981485
Abror <boqiyerabror@gmail.com>
2024-05-21 18:46:43 +0500

    second updating layout

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,13 @@
+export default function ProductDefaultLayout({ 
+    children,
+}: { 
+    children: React.ReactNode 
+}) {
+    return (
+        <div>
+            { children }
+            <h2 style={{backgroundColor: "yellowgreen", marginTop: '8rem'}}>Featured Products</h2>
+        </div>
+    ) 
+
+}
