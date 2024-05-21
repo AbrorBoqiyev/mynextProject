3314df60083b169a89ca32896417b8a165f99cfe
Abror <boqiyerabror@gmail.com>
2024-05-21 18:45:25 +0500

    Others page TXT

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function passwords() {
+    return (
+        <div>
+            <h1>This is passwords page</h1>
+        </div>
+    )
+}
---
+++ src/app/dashbord/page.tsx
@@ -0,0 +1,7 @@
+function BarChart() {
+    return <h1> Bar chart</h1>;
+}
+
+export default function Dashbord() {
+    return <h1>Dashbord</h1>
+}
