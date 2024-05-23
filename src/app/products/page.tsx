30cb748d6661a1fa3dea7e339234dbac3ad893b7
Abror <boqiyerabror@gmail.com>
2024-05-23 19:50:18 +0500

    small chages

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
+
+
 export default function ProductList(){
     return (
         <div>
+            <Link style={{textDecoration: 'none',color: "orange",fontSize: "16px",fontFamily: "sans-serif",fontWeight: "bolder",borderBottom: '2px solid orange',paddingBottom: '3px', margin: '20px',}} 
+             href={'/'}>Home</Link>
             <h1>Product List</h1>
             <h2>product 1</h2>
             <h2>product 2</h2>
